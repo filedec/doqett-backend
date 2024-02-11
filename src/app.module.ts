1fa2ad164b79967052b41db9c95ae1681512bb79
kamal2019 <mraryalkamal2011@gmail.com>
2024-02-11 12:22:46 +0545

    mongodb and config setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:".env"
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URI),
+    AuthModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
